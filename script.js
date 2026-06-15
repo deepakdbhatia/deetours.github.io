beaf4579a411e2add3cfcdc34f3989190c6a6ee2
deepakdbhatia <deepakdbhatia@gmail.com>
2026-06-15 02:03:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 function setLanguage(lang){
 
-alert(
-"German / English version will be connected in Step 2."
-);
+    alert(
+        "German language version will be connected in the next phase."
+    );
 
 }
+
+function acceptCookies(){
+
+    localStorage.setItem(
+        "cookieConsent",
+        "accepted"
+    );
+
+    document.getElementById(
+        "cookie-banner"
+    ).style.display="none";
+
+}
+
+function declineCookies(){
+
+    localStorage.setItem(
+        "cookieConsent",
+        "declined"
+    );
+
+    document.getElementById(
+        "cookie-banner"
+    ).style.display="none";
+
+}
+
+window.onload = function(){
+
+    if(
+        localStorage.getItem(
+            "cookieConsent"
+        )
+    ){
+
+        document.getElementById(
+            "cookie-banner"
+        ).style.display="none";
+
+    }
+
+};
